fe094c9525d6774bc78a685a294518c9bbc041ac
winklenurfer@gmail.com <alex.dickson@blackboard.com>
2014-08-31 22:45:16 -0700

    fixing route order

--- app/routes.js
+++ app/routes.js
@@ -4,24 +4,22 @@ module.exports = function(app) {
 	// handle things like api calls
 	// authentication routes
 
-	// frontend routes =========================================================
-	// route to handle all angular requests
-	app.get('*', function(req, res) {
-		res.sendfile('./public/index.html');
-	});
-
-	// sample api route
 	app.get('/api/ingredients', function(req, res) {
 		// use mongoose to get all ingredients in the database
-		console.log('routed to /api/ingredients');
 		Ingredient.find(function(err, ingredients) {
-			console.log('finding ' + ingredients);
 			// if there is an error retrieving, send the error. nothing after res.send(err) will execute
 			if (err)
 				res.send(err);
 
 			res.json(ingredients); // return all ingredients in JSON format
 		});
+	});
+
+
+	// frontend routes =========================================================
+	// route to handle all angular requests
+	app.get('*', function(req, res) {
+		res.sendfile('./public/index.html');
 	});
 
 };
