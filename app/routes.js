a6c8c0be6f68e648880e654491a42830081b58ce
winklenurfer@gmail.com <alex.dickson@blackboard.com>
2014-08-31 22:47:29 -0700

    including ingredients schema

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,7 @@
 module.exports = function(app) {
 
+	var Ingredient = require('./app/models/ingredient');
+	
 	// server routes ===========================================================
 	// handle things like api calls
 	// authentication routes
